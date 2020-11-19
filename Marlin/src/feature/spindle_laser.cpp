aeaf0326fc89e905732ceff6de06efeac066706d
Scott Lahteine <github@thinkyhead.com>
2020-11-19 00:00:52 -0600

    Fix undefined var

--- Marlin/src/feature/spindle_laser.cpp
+++ Marlin/src/feature/spindle_laser.cpp
@@ -39,7 +39,6 @@ uint8_t SpindleLaser::power;
 bool SpindleLaser::isReady;                                           // Ready to apply power setting from the UI to OCR
 cutter_power_t SpindleLaser::menuPower,                               // Power set via LCD menu in PWM, PERCENT, or RPM
                SpindleLaser::unitPower;                               // LCD status power in PWM, PERCENT, or RPM
-TERN_(LASER_POWER_INLINE, static uint8_t last_power_applied = 0);     // Pre init - used locally only
 TERN_(MARLIN_DEV_MODE, cutter_frequency_t SpindleLaser::frequency);   // PWM frequency setting; range: 2K - 50K
 
 #define SPINDLE_LASER_PWM_OFF ((SPINDLE_LASER_PWM_INVERT) ? 255 : 0)
@@ -90,8 +89,10 @@ void SpindleLaser::init() {
 // Set cutter ON/OFF state (and PWM) to the given cutter power value
 //
 void SpindleLaser::apply_power(const uint8_t opwr) {
+  static uint8_t last_power_applied = 0;
   if (opwr == last_power_applied) return;
   last_power_applied = opwr;
+
   power = opwr;
   #if ENABLED(SPINDLE_LASER_PWM)
     if (power == 0 && CUTTER_UNIT_IS(RPM)) {
